7c8778bc029437b5445691c404de1ecdb5e0a55b
Bryce Gold <bgold002@student.butte.edu>
2016-08-26 03:27:46 +0000

    lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,23 @@
+Algorithm
+
+- Computer randomly selects a number between 1 and 15
+
+- User inputs a single guess between 1 and 15
+
+- If user guess does not match computer's chosen number:
+
+    The user will lose a point from a starting 10 pts
+    
+    If the number chosen was too low:
+        > computer will display "higher" in a pop-up
+    
+    If the number chosen was too high:
+        > computer will display "lower" in a pop-up
+        
+    User guesses again until the guess matches the computer's chosen number
+        
+- Else If user guess matches computer's chosen number:
+    
+    The user gains 10 pts to their score
+    
+    Computer randomly chooses another number to guess
