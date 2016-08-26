014a8deb51e721c9eaf4242bae15c12af3fb380b
Bryce Gold <bgold002@student.butte.edu>
2016-08-26 02:51:35 +0000

    ASCII Art

---
+++ lab1/lab.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   std ::
+   cout << "      _^_     _^_" << endl;
+   cout << "     |   |   |   |" << endl; 
+   cout << "     | | |   | | |" << endl;
+   cout << "     | | |   | | |" << endl;
+   cout << "     | | |   | | |" << endl;
+   cout << "     -----   -----" << endl;
+   cout << "    {      ~      }" << endl;
+   cout << "   (               )" << endl;
+}
