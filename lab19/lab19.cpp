90bce9a4c873b82492e7fca78be203890c583ea3
Bryce Gold <bgold002@student.butte.edu>
2016-11-10 05:04:16 +0000

    Lab 19 Submission

--- lab19/lab19.cpp
+++ lab19/lab19.cpp
@@ -1,40 +1,72 @@
+/*
+** Bryce Gold
+**
+** 11/9/16
+**
+** This program takes in an input file of 4 columns of values and outputs a
+** separate file containing the values along with the sums for each row and 
+** sums for each column.
+*/
+
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <iomanip>
+#include <ios>
 
 using namespace std;
 
 int main() {
+    int myArray[125][4];
+    string fileName = " ";
+    
+    // variables used to store sums and overall total of rows and columns
     int NUM_OF_ROWS = 0;
-    int NUM_OF_COLS = 0;
-    int myArray[3][4];
-    float rowSums[3];
+    float rowSums[125];
     float colSums[4];
     float rowTotal = 0;
     float colTotal = 0;
     float matrixAvg = 0;
+    
     ifstream inFS;
     ofstream  outFS;
     
-    inFS.open("initial.txt");
+    cout << "Input the file name: ";
+    cin  >> fileName;
     
-    // stores the input file in the array
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            inFS >> myArray[i][j];
+    cout << "Opening file..." << endl;
+    
+    inFS.open(fileName);
+    outFS.open("output.txt");
+    
+    // returns error if cannot open user file
+    if (!inFS.is_open()) {
+        cout << "Could not open the file" << endl;
+        return 1;
+    }
+
+    int row = 0;
+    int col = 0;
+
+    // stores the input file values into the array
+    for (row = 0; !inFS.eof(); ++row) {
+        
+        for (int col = 0; col < 4; ++col) {
+            inFS >> myArray[row][col];
+            
+            NUM_OF_ROWS = row;
         }
     }
     
-    int row = 0;
-    int col = 0;
-    
     // loops to find total of each row and save it in the rowSums array
-    for (row = 0; row < 3; ++row) {
-        for (col = 0; col < 4; ++col){
+    for (row = 0; row <= NUM_OF_ROWS; ++row) {
+        for (col = 0; col < 4; ++col) {
             rowTotal += myArray[row][col];
-        
+            
             if (col == 3) {
                 rowSums[row] = rowTotal;
                 matrixAvg += rowTotal;
+                
                 rowTotal = 0;
             }
         }
@@ -42,30 +74,52 @@ int main() {
     
     // loops to find total of each column and save it to the colSums array
     for (col = 0; col < 4; ++col) {
-        for (row = 0; row < 3; ++row){
-            colTotal += myArray[row][col];
         
-            if (row == 2) {
+        for (row = 0; row <= NUM_OF_ROWS; ++row){
+            colTotal += myArray[row][col];
+            
+            if (row == NUM_OF_ROWS) {
                 colSums[col] = colTotal;
                 matrixAvg += colTotal;
+                
                 colTotal = 0;
             }
         }
     }
+
+    cout << "Writing to output file..." << endl;
+    
+    // loop outputs the original values and total for each row in the output file
+    for (row = 0; row <= NUM_OF_ROWS; ++row) {
+        for (col = 0; col < 4; ++col) {
+            
+            outFS << myArray[row][col] << setfill(' ') << setw(4);
+            
+            if (col == 3) {
+                outFS << setfill('.') << setw(4) << rowSums[row];
+                outFS << endl;
+            }
+        }
+    }
+    
+    for (row = 0; row < 4; ++row) {
+        outFS << ":" << setfill(' ') << setw(4);
+    }
+    
+    outFS << endl << left;
+    
+    // outputs the total for each column along bottom of each column in output file
+    for (row = 0; row < 4; ++row) {
+        outFS << colSums[row] << setfill(' ') << setw(4);
+    }
     
     // finds the average of all the numbers
-    matrixAvg = matrixAvg / 12;
+    matrixAvg = matrixAvg / (NUM_OF_ROWS * 4);
 
-    cout << "rows: " << endl;
-    cout << rowSums[0] << endl;
-    cout << rowSums[1] << endl;
-    cout << rowSums[2] << endl;
-    cout << "cols:" << endl;
-    cout << colSums[0] << endl;
-    cout << colSums[1] << endl;
-    cout << colSums[2] << endl;
-    cout << colSums[3] << endl;
-    cout << "matrixAvg: " << endl;
-    cout << matrixAvg << endl;
-    
+    outFS << endl << "Average of all numbers: " << matrixAvg;
+
+    cout << "Closing input and output files." << endl;
+    inFS.close();
+    outFS.close();
+
 }
